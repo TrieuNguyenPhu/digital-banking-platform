2b38af8b8abadec60db8742500adabb5943927e3
NguyenPhuTrieu <nguyentrieu080604@gmail.com>
2026-08-24 20:25:24 +0700

    refactor: rename base package

--- src/main/java/io/github/trieunguyenphu/digitalbanking/DigitalBankingPlatformApplication.java
+++ src/main/java/io/github/trieunguyenphu/digitalbanking/DigitalBankingPlatformApplication.java
@@ -1,4 +1,4 @@
-package io.github.trieunguyenphu.digitalbankingplatform;
+package io.github.trieunguyenphu.digitalbanking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/io/github/trieunguyenphu/digitalbanking/DigitalBankingPlatformApplicationTests.java
+++ src/test/java/io/github/trieunguyenphu/digitalbanking/DigitalBankingPlatformApplicationTests.java
@@ -1,4 +1,4 @@
-package io.github.trieunguyenphu.digitalbankingplatform;
+package io.github.trieunguyenphu.digitalbanking;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
