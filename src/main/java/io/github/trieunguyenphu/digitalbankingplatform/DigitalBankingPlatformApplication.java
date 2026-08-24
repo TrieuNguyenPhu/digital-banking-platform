d27a9c7c41cc5da334d1da483c77c942c4b71f81
NguyenPhuTrieu <nguyentrieu080604@gmail.com>
2026-08-24 20:13:11 +0700

    chore: initialize Spring Boot project

---
+++ src/main/java/io/github/trieunguyenphu/digitalbankingplatform/DigitalBankingPlatformApplication.java
@@ -0,0 +1,13 @@
+package io.github.trieunguyenphu.digitalbankingplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalBankingPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DigitalBankingPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/trieunguyenphu/digitalbankingplatform/DigitalBankingPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.trieunguyenphu.digitalbankingplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalBankingPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
